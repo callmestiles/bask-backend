d515403a61627d96ec73d7aaefb6886bd723c0c5
callmestiles <callmestiless@gmail.com>
2025-11-01 12:49:54 +0200

    start up basic server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello, World!");
+import express, { Express } from "express";
+
+const app: Express = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello, Bask Backend!");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
+});
